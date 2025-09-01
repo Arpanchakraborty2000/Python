c4fc82553ca1f9923499e2b108130f836d701203
Arpan Chakraborty <chakrabortyarpan1212@gmail.com>
2025-09-01 20:58:14 +0530

    numpy

---
+++ Numpy/numpy-code.py
@@ -0,0 +1,7 @@
+import numpy as np 
+
+lst = [1, 2, 3, 4, 5]      # A normal Python list
+arr = np.array(lst)        # Convert list to NumPy array
+
+print(arr)                 # Output: [1 2 3 4 5]
+print(type(arr))           # Output: <class 'numpy.ndarray'>
